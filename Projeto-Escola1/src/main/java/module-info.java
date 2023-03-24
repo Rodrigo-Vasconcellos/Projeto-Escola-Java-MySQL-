7b75d45532e88f79e50d6f13b9e00f1a53a7f478
Rodrigo Vasconcellos <114500223+Rodrigo-Vasconcellos@users.noreply.github.com>
2023-03-23 22:03:42 -0300

    delete coisas antigas

--- Projeto-Escola1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.projetoescola1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.projetoescola1 to javafx.fxml;
-    exports com.example.projetoescola1;
-}
