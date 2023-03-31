398a2095587872724572f20ea9e8dab419648da3
Rodrigo Vasconcellos <114500223+Rodrigo-Vasconcellos@users.noreply.github.com>
2023-03-31 18:08:19 -0300

    Delete telasEscola directory
    foi deletado para colocar um atualizado de outra maquina

--- telasEscola/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.telasescola {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.telasescola to javafx.fxml;
-    exports com.example.telasescola;
-}
